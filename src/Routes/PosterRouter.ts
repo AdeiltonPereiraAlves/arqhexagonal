9bb058b59caa413c1c2f6200faee9edf8357cf60
unknown <adeutoo@gmail.com>
2024-11-06 21:17:51 -0300

    add excluir post

--- src/Routes/PosterRouter.ts
+++ src/Routes/PosterRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import InserirPostController from "../controllers/InserirPostController";
 import BuscarPostsController from "../controllers/BuscarPostsController";
 import BuscarPostController from "../controllers/BuscarPostController"
+import DeletarPostController from "../controllers/DeletarPostController";
 import { VerificarToken } from "../adptadores/middleware/Token";
 
 const router = Router()
@@ -9,6 +10,6 @@ const router = Router()
 router.post("/", VerificarToken, InserirPostController.inserir)
 router.get("/", BuscarPostsController.buscarPosts)
 router.get("/detalhes/:id",BuscarPostController.buscarPorId)
-
+router.delete("/excluir/:id",DeletarPostController.deletar )
 
 export default router
--- src/controllers/BuscarPostController.ts
+++ src/controllers/BuscarPostController.ts
@@ -3,7 +3,7 @@ import ObterPostID from "../core/posts/ObterPostPorID";
 import PostDb from "../adptadores/db/PostDb";
 
 export default class BuscarPostController {
-  static async buscarPorId(req: any, res:any){
+  static async buscarPorId(req: Request, res: Response):Promise<Response | any>{
     try {
       const id = req.params.id;
       const buscarPost = new ObterPostID(new PostDb());
@@ -14,8 +14,8 @@ export default class BuscarPostController {
       console.log(post);
       res.json(post);
     } catch (error) {
-        console.error("Erro ao buscar post no controller:", error);
-        return res.status(500).send("Erro ao buscar post.");
+      console.error("Erro ao buscar post no controller:", error);
+      return res.status(500).send("Erro ao buscar post.");
     }
   }
 }
